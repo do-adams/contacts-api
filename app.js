5a3555d611f2c65dc4dfed10b26aa1b9a2beec09
Damián Adams <damian@polygraphmedia.com>
2018-08-18 18:44:40 -0400

    Remove jade dependencies and files, and all view files and references

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const createError = require('http-errors'),
   cookieParser = require('cookie-parser'),
   logger = require('morgan');
 
-const indexRouter = require('./routes/index'),
-  usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
 const app = express();
 
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,7 +35,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send(err.message);
 });
 
 module.exports = app;
