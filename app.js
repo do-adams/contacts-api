f01c5a76631d2accf74e9fddc9ec85e5d08851c6
Damián Adams <contact@mianlabs.com>
2018-08-18 21:15:35 -0400

    Add the contacts module and data store

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // render the error
   res.status(err.status || 500);
   res.send(err.message);
 });
