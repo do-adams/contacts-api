545e3ed07b4c86095bd44919733557048c2a2a5b
Damián Adams <contact@mianlabs.com>
2018-08-18 22:55:33 -0400

    Create initial contact routes

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const createError = require('http-errors'),
   cookieParser = require('cookie-parser'),
   logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-
-const app = express();
+const app = express(),
+  contacts = require('./modules/contacts');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +19,38 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.get('/contacts', 
+		function(request, response){
+			var get_params = request.query;
+			
+			if (Object.keys(get_params).length === 0)
+			{
+				response.setHeader('content-type', 'application/json');
+				response.end(JSON.stringify(contacts.list()));	
+			}
+			else
+			{
+				response.setHeader('content-type', 'application/json');
+				response.end(JSON.stringify(contacts.query_by_arg(get_params.arg, get_params.value)));
+			}
+		}
+);
+
+
+app.get('/contacts/:number', function(request, response) {
+	response.setHeader('content-type', 'application/json');	
+	response.end(JSON.stringify(contacts.query(request.params.number)));
+});
+
+app.get('/groups', function(request, response) {
+	response.setHeader('content-type', 'application/json');	
+	response.end(JSON.stringify(contacts.list_groups()));
+});
+
+app.get('/groups/:name', function(request, response) {
+	response.setHeader('content-type', 'application/json');	
+	response.end(JSON.stringify(contacts.get_members(request.params.name)));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
